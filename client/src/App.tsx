abec49b844e85c8ed6363e93d65c7a2ce9411dfb
Enzo CORSICO <58816644+enzocorsico@users.noreply.github.com>
2022-11-05 19:09:58 +0100

    Init tailwindcss

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,9 +14,9 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+      <h1 className='text-2xl font-bold'>Vite + React</h1>
+      <div>
+        <button className="border-2" onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
