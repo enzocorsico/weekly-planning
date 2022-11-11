8d9ce7eae2b1cd6e13065d670b9ca303ee9c7c8b
Enzo CORSICO <58816644+enzocorsico@users.noreply.github.com>
2022-11-11 14:41:20 +0100

    Routes task finito

--- back/src/task/dto/create-task.dto.ts
+++ back/src/task/dto/create-task.dto.ts
@@ -1,1 +1,24 @@
-export class CreateTaskDto {}
+import { IsNotEmpty } from "class-validator";
+
+export class CreateTaskDto {
+    @IsNotEmpty()
+    name: string;
+
+    @IsNotEmpty()
+    description: string;
+
+    @IsNotEmpty()
+    startDate: Date;
+
+    @IsNotEmpty()
+    endDate: Date;
+
+    @IsNotEmpty()
+    typeId: number;
+
+    @IsNotEmpty()
+    priorityId: number;
+
+    @IsNotEmpty()
+    agendaId: number;
+}
